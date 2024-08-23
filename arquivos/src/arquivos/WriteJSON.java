d1d74c7552242aca562d67734826023cfc5252a5
marti <andremartins.a@outlook.com>
2024-08-23 14:46:55 -0300

    reading a Json File

--- arquivos/src/arquivos/WriteJSON.java
+++ arquivos/src/arquivos/WriteJSON.java
@@ -1,5 +1,6 @@
 package arquivos;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,6 +8,9 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class WriteJSON {
 
@@ -38,9 +42,27 @@ public class WriteJSON {
         
         // Escrevendo o JSON no arquivo
         FileWriter fileWriter = new FileWriter("C:\\Users\\marti\\git\\repository5\\arquivos\\src\\arquivos\\fileJson.json");
-        
         fileWriter.write(jsonUser);
         fileWriter.flush();
         fileWriter.close();
+        
+        //****************************READING A JSON FILE ***********************************     
+        FileReader fileReader = new FileReader("C:\\Users\\marti\\git\\repository5\\arquivos\\src\\arquivos\\fileJson.json");
+        
+        // Parseando o JSON do arquivo para um JsonArray
+        JsonArray jsonArray = JsonParser.parseReader(fileReader).getAsJsonArray();
+        List<User> users1 = new ArrayList<User>();
+        
+        // Percorrendo cada JsonElement do JsonArray e desserializando para o objeto User
+        for (JsonElement jsonElement : jsonArray) {
+            User user = gson.fromJson(jsonElement, User.class);
+            users1.add(user);
+        }
+        
+        // Fechar o FileReader
+        fileReader.close();
+        
+        // Exibindo os usuários lidos do JSON
+        System.out.println("Json File Read: " + users1);
     }
 }
